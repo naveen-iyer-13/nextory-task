ecb87cab3ade05899c4dc312e1ab639b10214658
naveen-iyer-13 <naveeniyer13@gmail.com>
2020-04-22 00:23:12 +0530

    Updates

---
+++ precache-manifest.a92ec8b7fe556b2c4d9fadce7cd15654.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a318eccf1f9cb23fe0c56adacc6dbcc6",
+    "url": "/nextory-task/index.html"
+  },
+  {
+    "revision": "ba792f394eb3eea1a494",
+    "url": "/nextory-task/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "a4d8eaf6c5fe0a66fa82",
+    "url": "/nextory-task/static/css/main.c8f82be0.chunk.css"
+  },
+  {
+    "revision": "ba792f394eb3eea1a494",
+    "url": "/nextory-task/static/js/2.4e4dcf41.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nextory-task/static/js/2.4e4dcf41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d8eaf6c5fe0a66fa82",
+    "url": "/nextory-task/static/js/main.5e9212e2.chunk.js"
+  },
+  {
+    "revision": "f74b3dab78af4e91b668",
+    "url": "/nextory-task/static/js/runtime-main.4eb9b0a7.js"
+  }
+]);
